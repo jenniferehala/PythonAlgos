0ad9406799803e964a40a627c969506ca117826b
jehala <jenniferehala@github.com>
2022-03-29 14:32:22 -0400

    commit

--- 13_RecursiveIntro.js
+++ 13_RecursiveIntro.js
@@ -1,6 +1,6 @@
 function sigma(num) {
     var sum = 0
-    for (i = 0 ; i <= num ; i ++) {
+    for (i = 0; i <= num; i++) {
         sum += i
     }
     return sum
@@ -11,7 +11,7 @@ function recursiveSigma(num) {
     if (num <= 1) {
         return 1
     }
-    
+
     return num * recursiveSigma(num - 1)
 }
 
